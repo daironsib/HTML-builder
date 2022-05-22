a38f3300a7f5b624a67c9e27ae3364271999b27e
Artem Sokolov <daironsib@gmail.com>
2022-05-22 21:41:02 +0200

    fix: update solution for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = process;
 
 let stream = fs.createReadStream(path.join(__dirname, 'text.txt'));
 
 stream.on('data', function (chunk) {
-  console.log(chunk.toString());
+  stdout.write(chunk.toString());
 });
