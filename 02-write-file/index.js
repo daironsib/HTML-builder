77a800eba8ecbc55a2cd4975365429a603961eaf
Dairon <daironsib@gmail.com>
2022-05-26 10:49:16 +0200

    fix: update solution for 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,9 +5,10 @@ const { stdin, stdout } = process;
 stdout.write('Введите текст\n');
 const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 stdin.on('data', data => {
-  data.toString() === `exit\n` ? process.emit('SIGINT') : file.write(data.toString());
+  data.toString().trim() === 'exit' ? process.emit('SIGINT') : file.write(data.toString());
 });
+
 process.on('SIGINT', () => {
-  stdout.write('Удачи!');
+  stdout.write('Удачи и хорошего дня!');
   process.exit();
 });
